7666b008ff05d89d844b5b986edebe227b5bbf13
thodoras <theodoretopol@gmail.com>
2024-04-14 14:38:28 +0300

    init

---
+++ src/main/java/org/thodoras/eazyschool/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package org.thodoras.eazyschool.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value={"/home", "", "/"})
+    public String displayHomePage() {
+        return "home.html";
+    }
+}
